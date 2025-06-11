32371ea50870d1cd889ee1671340bafada791383
Ancientshield <jacklin2100@gmail.com>
2025-06-11 22:02:22 +0800

    import TailwindCSS and add pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
-})
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
+	alias: { '@': resolve(__dirname, '/') },
+	modules: ['@nuxt/eslint', '@nuxt/ui'],
+	css: ['~/assets/css/main.css'],
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>Hello, this is the home page.</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
