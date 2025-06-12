b79a3e0c7330e70c250e1a78ddba2be2d8501070
Ancientshield <jacklin2100@gmail.com>
2025-06-12 16:27:27 +0800

    add .env and setting nuxt.config.ts

--- components/movie/search.vue
+++ components/movie/search.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-	const config = useRuntimeConfig();
-	const apiKey = config.public.OMDB_API_KEY;
+	const runtimeConfig = useRuntimeConfig();
+	const apiKey = runtimeConfig.public.omdbApiKey;
 	const query = ref('');
 	interface IMovies {
 		Title: string;
@@ -19,29 +19,27 @@
 </script>
 
 <template>
-	<form @submit.prevent="searchMovieById">
-		<UInput v-model="query" class="m-3" type="text" />
-		<UButton @click="searchMovieById">Search</UButton>
-	</form>
-	<ul class="flex flex-wrap gap-10 list-none">
-		<li v-for="movie in movies" :key="movie.imdbID">
-			<NuxtLink :to="`/movies/${movie.imdbID}`">
-				<UCard class="w-64">
-					<UCardHeader>
+	<div>
+		<form @submit.prevent="searchMovieById">
+			<UInput v-model="query" class="m-3" type="text" />
+			<UButton @click="searchMovieById">Search</UButton>
+		</form>
+		<ul class="flex flex-wrap gap-10 list-none">
+			<li v-for="movie in movies" :key="movie.imdbID">
+				<NuxtLink :to="`/movies/${movie.imdbID}`">
+					<UCard class="w-64">
 						<h3 class="text-base font-bold">{{ movie.Title }}</h3>
 						<p class="text-sm text-gray-600">{{ movie.Year }}</p>
-					</UCardHeader>
-					<UCardBody>
 						<img
 							:src="movie.Poster"
 							:alt="movie.Title"
 							class="w-full h-64 object-cover"
 						/>
-					</UCardBody>
-				</UCard>
-			</NuxtLink>
-		</li>
-	</ul>
+					</UCard>
+				</NuxtLink>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	runtimeConfig: {
+		public: {
+			omdbApiKey: process.env.OMDB_API_KEY,
+		},
+	},
 	compatibilityDate: '2025-05-15',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint', '@nuxt/ui'],
