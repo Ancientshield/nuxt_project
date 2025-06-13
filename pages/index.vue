a976e3d0ab06df278944722acdd863475e32f386
Ancientshield <jacklin2100@gmail.com>
2025-06-13 15:13:15 +0800

    remove scroll-y

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
-	<div class="flex flex-col items-center min-h-screen">
-		<div class="w-full flex flex-col items-center">
-			<h1 class="text-3xl font-bold mb-1">Home</h1>
-			<UCard class="max-w-xs w-full mb-5">
-				<form
-					v-if="!isLoggedIn"
-					class="flex flex-col gap-4"
-					@submit.prevent="login"
-				>
-					<UInput v-model="username" placeholder="account" />
-					<UInput v-model="password" type="password" placeholder="password" />
-					<UButton type="submit" color="primary" block>Login</UButton>
-				</form>
-				<div v-else class="text-center">
-					<div class="mb-2">Already logged in</div>
-					<UButton color="primary" @click="goMovies">前往 Movies 頁面</UButton>
-					<UButton class="ml-2" @click="logout">Logout</UButton>
-				</div>
-			</UCard>
-		</div>
+	<div class="flex flex-col items-center">
+		<h1 class="text-3xl font-bold mb-1">Home</h1>
+		<UCard>
+			<form
+				v-if="!isLoggedIn"
+				class="flex flex-col gap-4"
+				@submit.prevent="login"
+			>
+				<UInput v-model="username" placeholder="account" />
+				<UInput v-model="password" type="password" placeholder="password" />
+				<UButton type="submit" color="primary" block>Login</UButton>
+			</form>
+			<div v-else class="text-center">
+				<div class="mb-2">Already logged in</div>
+				<UButton color="primary" @click="goMovies">前往 Movies 頁面</UButton>
+				<UButton class="ml-2" @click="logout">Logout</UButton>
+			</div>
+		</UCard>
 	</div>
 </template>
 
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="justify-center">
 		<div class="flex flex-col items-center">
-			<h1 class="text-3xl font-bold">Movies</h1>
-			<p class="text-gray-600">Search for your favorite movies</p>
+			<h1 class="text-3xl font-bold mb-1">Movies</h1>
 		</div>
 		<div v-if="!isLoggedIn">
-			<UCard class="max-w-xs w-full mx-auto mt-8 text-center">
+			<UCard class="max-w-xs mx-auto text-center">
 				<p class="text-red-500">Please log in to search!</p>
 			</UCard>
 		</div>
 		<div v-else>
+			<p class="text-gray-600">Search for your favorite movies</p>
 			<MovieSearch />
 		</div>
 	</div>
